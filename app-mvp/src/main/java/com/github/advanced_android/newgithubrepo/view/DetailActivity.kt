cdcd1f02232262352edc71736635d6bc22502b08
y-zumi <b1501498@planet.kanazawa-it.ac.jp>
2018-01-10 18:04:24 +0900

    Format DetailActivity.kt

--- app-mvp/src/main/java/com/github/advanced_android/newgithubrepo/view/DetailActivity.kt
+++ app-mvp/src/main/java/com/github/advanced_android/newgithubrepo/view/DetailActivity.kt
@@ -6,13 +6,11 @@ import android.graphics.Bitmap
 import android.net.Uri
 import android.os.Bundle
 import android.support.design.widget.Snackbar
-import android.support.v4.graphics.drawable.RoundedBitmapDrawable
 import android.support.v4.graphics.drawable.RoundedBitmapDrawableFactory
 import android.support.v7.app.AppCompatActivity
 import android.view.View
 import android.widget.ImageView
 import android.widget.TextView
-
 import com.bumptech.glide.Glide
 import com.bumptech.glide.request.target.BitmapImageViewTarget
 import com.github.advanced_android.newgithubrepo.R
@@ -24,13 +22,13 @@ import com.github.advanced_android.newgithubrepo.presenter.DetailPresenter
  * 詳細画面を表示するActivity
  */
 class DetailActivity : AppCompatActivity(), DetailContract.View {
-    private var fullNameTextView: TextView? = null
-    private var detailTextView: TextView? = null
-    private var repoStarTextView: TextView? = null
-    private var repoForkTextView: TextView? = null
-    private var ownerImage: ImageView? = null
-    private var detailPresenter: DetailContract.UserActions? = null
-    private var fullRepoName: String? = null
+    private lateinit var fullNameTextView: TextView
+    private lateinit var detailTextView: TextView
+    private lateinit var repoStarTextView: TextView
+    private lateinit var repoForkTextView: TextView
+    private lateinit var ownerImage: ImageView
+    private lateinit var detailPresenter: DetailContract.UserActions
+    private lateinit var fullRepoName: String
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -38,16 +36,16 @@ class DetailActivity : AppCompatActivity(), DetailContract.View {
         val intent = intent
         fullRepoName = intent.getStringExtra(EXTRA_FULL_REPOSITORY_NAME)
 
-        fullNameTextView = this@DetailActivity.findViewById(R.id.fullname) as TextView?
-        detailTextView = findViewById(R.id.detail) as TextView?
-        repoStarTextView = findViewById(R.id.repo_star) as TextView?
-        repoForkTextView = findViewById(R.id.repo_fork) as TextView?
-        ownerImage = findViewById(R.id.owner_image) as ImageView?
+        fullNameTextView = this@DetailActivity.findViewById(R.id.fullname) as TextView
+        detailTextView = findViewById(R.id.detail) as TextView
+        repoStarTextView = findViewById(R.id.repo_star) as TextView
+        repoForkTextView = findViewById(R.id.repo_fork) as TextView
+        ownerImage = findViewById(R.id.owner_image) as ImageView
 
 
         val gitHubService = (application as NewGitHubReposApplication).gitHubService
         detailPresenter = DetailPresenter(this as DetailContract.View, gitHubService)
-        detailPresenter!!.prepare()
+        detailPresenter.prepare()
     }
 
     override fun getFullRepositoryName(): String? {
@@ -55,24 +53,24 @@ class DetailActivity : AppCompatActivity(), DetailContract.View {
     }
 
     override fun showRepositoryInfo(response: GitHubService.RepositoryItem) {
-        fullNameTextView!!.text = response.full_name
-        detailTextView!!.text = response.description
-        repoStarTextView!!.text = response.stargazers_count
-        repoForkTextView!!.text = response.forks_count
+        fullNameTextView.text = response.full_name
+        detailTextView.text = response.description
+        repoStarTextView.text = response.stargazers_count
+        repoForkTextView.text = response.forks_count
         // サーバーから画像を取得してimageViewに入れる
         Glide.with(this@DetailActivity)
                 .load(response.owner.avatar_url)
-                .asBitmap().centerCrop().into(object : BitmapImageViewTarget(ownerImage!!) {
+                .asBitmap().centerCrop().into(object : BitmapImageViewTarget(ownerImage) {
             override fun setResource(resource: Bitmap) {
                 val circularBitmapDrawable = RoundedBitmapDrawableFactory.create(resources, resource)
                 circularBitmapDrawable.isCircular = true
-                ownerImage!!.setImageDrawable(circularBitmapDrawable)
+                ownerImage.setImageDrawable(circularBitmapDrawable)
             }
         })
         // ロゴとリポジトリの名前をタップしたら、作者のGitHubのページをブラウザで開く
-        val listener = View.OnClickListener { detailPresenter!!.titleClick() }
-        fullNameTextView!!.setOnClickListener(listener)
-        ownerImage!!.setOnClickListener(listener)
+        val listener = View.OnClickListener { detailPresenter.titleClick() }
+        fullNameTextView.setOnClickListener(listener)
+        ownerImage.setOnClickListener(listener)
     }
 
     /**
